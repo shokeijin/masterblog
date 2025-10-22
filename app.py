72c8b5adb6c39a8991b22ee157638e9ba4f05944
Andreas <shokeijin@gmail.com>
2025-10-22 17:01:56 +0200

    Datenstruktur entworfen und code angepasst

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Öffne die JSON-Datei im Lesemodus ('r')
+    with open('blog_posts.json', 'r') as f:
+        # Lade den Inhalt der Datei in die Variable 'blog_posts'
+        blog_posts = json.load(f)
+
+    # Gib die Variable 'blog_posts' unter dem Namen 'posts' an das Template weiter
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
